6fc568b0d2ecefc55d5da2ef8cc2b72bb215909f
Abdul Rehamn <92205472+likeam@users.noreply.github.com>
2025-02-18 20:59:52 +0500

    Removing Error

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -56,7 +56,7 @@ const Message = () => {
           </div>
           <div class="mb-6">
             <label class="block text-gray-700 text-sm font-bold mb-2  dark:text-gray-200">
-              Message
+              Messages
             </label>
             <textarea
               class="shadow appearance-none border border-gray-300 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline  dark:text-gray-200"
